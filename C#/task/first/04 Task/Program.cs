57505e5fcc967904464e236b101371267609be6c
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 17:33:43 +0300

    04 T Line even numbers

---
+++ C#/task/first/04 Task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число... ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= number; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}");
+        if (!(i + 1 == number || i == number)) Console.Write(", ");
+    }
+}
+Console.Write("\n");
