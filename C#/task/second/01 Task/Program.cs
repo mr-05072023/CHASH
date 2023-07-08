c7b0ada094f87bede147c393c5fff3f1989eff83
mr-05072023 <mr.05072023@gmail.com>
2023-07-09 00:44:00 +0300

    01 T Search second digit

---
+++ C#/task/second/01 Task/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трёхзначное число... ");
+int number = int.Parse(Console.ReadLine());
+
+int s_number = number / 10 % 10;
+
+Console.WriteLine($"Вторая цифра числа {number} равна {s_number}");
