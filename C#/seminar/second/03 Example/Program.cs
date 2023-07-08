82464fd52d77c73e4f56da2b0d058ce3e705fdb0
mr-05072023 <mr.05072023@gmail.com>
2023-07-09 00:08:24 +0300

    03 Ex First multiple second number

---
+++ C#/seminar/second/03 Example/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число... ");
+int first_number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число... ");
+int second_number = int.Parse(Console.ReadLine());
+
+int digit = first_number % second_number;
+
+if (digit == 0)
+{
+    Console.WriteLine($"Первое число {first_number} кратно второму числу {second_number}");
+}
+else
+{
+    Console.WriteLine($"Первое число {first_number} не кратно второму числу {second_number}, остаток равен {digit}");
+}
