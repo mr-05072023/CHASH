823c8166844f36b07950c37f794436be88eb6eec
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 17:22:29 +0300

    03 T Even or odd

---
+++ C#/task/first/03 Task/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число... ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} чётное");
+}
+else
+{
+    Console.WriteLine($"Число {number} нечётное");
+}
