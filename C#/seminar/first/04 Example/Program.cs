1b61bc93fe9ec6fea320ed84813c1d7b2a1c5d02
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 16:43:52 +0300

    04_01 New implementation through //for

--- C#/seminar/first/04 Example/Program.cs
+++ C#/seminar/first/04 Example/Program.cs
@@ -3,22 +3,32 @@ int number = int.Parse(Console.ReadLine());
 
 if (number > 0)
 {
-    int count = number * -1;
+    int count = -number;
     while (number >= count)
     {
         Console.Write(count + " ");
         count++;
     }
+    // or
+    // for (int count = -number; number >= count; count++)
+    // {
+    //     Console.Write(count + " ");
+    // }
     Console.WriteLine();
 }
 else if (number < 0)
 {
-    int count = number * -1;
+    int count = -number;
     while (count >= number)
     {
         Console.Write(count + " ");
         count--;
     }
+    // or
+    // for (int count = -number; count >= number; count--)
+    // {
+    //     Console.Write(count + " ");
+    // }
     Console.WriteLine();
 }
 else
