7712610642a5c0517d383c730f31ab75b832f9fb
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 16:52:37 +0300

    01 T Max number

---
+++ C#/task/first/01 Task/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число... ");
+int first_number = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число... ");
+int second_number = int.Parse(Console.ReadLine());
+if (first_number > second_number) Console.WriteLine($"{first_number} больше чем {second_number}");
+if (first_number < second_number) Console.WriteLine($"{second_number} больше чем {first_number}");
