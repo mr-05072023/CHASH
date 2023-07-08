00469901ce84e5246327dd8a6ca8a2d0354c0b8f
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 23:56:28 +0300

    02 Ex Two-digit from three-digit

---
+++ C#/seminar/second/02 Example/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(100, 1000);
+
+// int first_number = number / 100;
+// int third_numbers = number % 10;
+// Console.WriteLine($"Число {number} без средней цифры равно {first_number}{third_numbers}");
+
+// OR
+
+int first_number = number / 100 * 10;
+int third_numbers = number % 10;
+int result = first_number + third_numbers;
+Console.WriteLine($"Число {number} без средней цифры равно {result}");
