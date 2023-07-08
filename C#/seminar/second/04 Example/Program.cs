9dc3d440cbb5c11d7d0b4fadebf6c20a9a3f15d5
mr-05072023 <mr.05072023@gmail.com>
2023-07-09 00:18:32 +0300

    04 Ex Multiple to 7 and 23

---
+++ C#/seminar/second/04 Example/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число... ");
+int number = int.Parse(Console.ReadLine());
+int first_divin = number % 7;
+int second_divin = number % 23;
+
+if (first_divin == 0 && second_divin == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно 7 и 23");
+}
