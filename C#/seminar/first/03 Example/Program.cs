e6173fa1ccc49a0f67653db18b04e0a6f3fa1180
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 15:25:08 +0300

    03 Ex Week number

---
+++ C#/seminar/first/03 Example/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите номер дня недели... ");
+int week_number = int.Parse(Console.ReadLine());
+
+if (week_number == 1) Console.WriteLine("Понедельник");
+if (week_number == 2) Console.WriteLine("Вторник");
+if (week_number == 3) Console.WriteLine("Среда");
+if (week_number == 4) Console.WriteLine("Четверг");
+if (week_number == 5) Console.WriteLine("Пятница");
+if (week_number == 6) Console.WriteLine("Суббота");
+if (week_number == 7) Console.WriteLine("Воскресенье");
+
+switch (week_number) 
+{
+    case 1:
+       Console.WriteLine("Понедельник");
+       break;
+    case 2:
+       Console.WriteLine("Вторник");
+       break;
+    case 3:
+       Console.WriteLine("Среда");
+       break;
+    case 4:
+       Console.WriteLine("Четверг");
+       break;
+    case 5:
+       Console.WriteLine("Пятница");
+       break;
+    case 6:
+       Console.WriteLine("Суббота");
+       break;
+    case 7:
+       Console.WriteLine("Воскресенье");
+       break; 
+}
