01374152ee12420bd2d07d48152dffa76eab5d62
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 16:29:17 +0300

    04 Ex Set of integers

---
+++ C#/seminar/first/04 Example/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число... ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = number * -1;
+    while (number >= count)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+    Console.WriteLine();
+}
+else if (number < 0)
+{
+    int count = number * -1;
+    while (count >= number)
+    {
+        Console.Write(count + " ");
+        count--;
+    }
+    Console.WriteLine();
+}
+else
+{
+    Console.Write($"{number} \n");
+}
