1c3902baec379b1b733306e20657b275f8178a5b
mr-05072023 <mr.05072023@gmail.com>
2023-07-09 00:59:03 +0300

    02 T Search thrid digit

---
+++ C#/task/second/02 Task/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число... ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99) 
+{
+    while (number > 1000)
+    {
+        number /= 10;
+    }
+    number %= 10;
+    Console.WriteLine($"Третья цифра равна {number}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
