69d12f7d33e12e055e98c3b9eb6f9d29372c8288
mr-05072023 <mr.05072023@gmail.com>
2023-07-09 01:09:50 +0300

    03 T Search weekend day

---
+++ C#/task/second/03 Task/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите цифру дня недели... ");
+int number_day = int.Parse(Console.ReadLine());
+
+string SearchDayWeek(int number)
+{
+    return number == 6 || number == 7 ? 
+    "Выходной день" : 
+    "Будний день";
+}
+
+Console.WriteLine(SearchDayWeek(number_day));
