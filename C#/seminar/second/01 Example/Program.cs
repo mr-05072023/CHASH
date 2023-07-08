86db688d160c15538e30a2e5c796d410bb150343
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 23:41:54 +0300

    01 Ex Search max value in number on segment

---
+++ C#/seminar/second/01 Example/Program.cs
@@ -0,0 +1,15 @@
+﻿int random_number = new Random().Next(10, 100);
+int first_value = random_number / 10;
+int second_value = random_number % 10;
+if (first_value > second_value)
+{
+    Console.WriteLine($"{first_value} максимальная цифра числа {random_number}");
+}
+else if (first_value < second_value)
+{
+    Console.WriteLine($"{second_value} максимальная цифра числа {random_number}");
+}
+else
+{
+    Console.WriteLine($"Цифры числа {random_number} равны");
+}
