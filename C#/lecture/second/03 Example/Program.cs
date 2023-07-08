0af770854e8efc8fc5084dbb184565a3bd3ac195
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 20:23:50 +0300

    03 Ex Search number in array

---
+++ C#/lecture/second/03 Example/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array_numbers = {1, 15, 31, 4, 15, 16, 17, 18};
+
+int number = array_numbers.Length;
+Console.Write("Введите число... ");
+int find = int.Parse(Console.ReadLine());
+
+int index = 0;
+
+while (index < number)
+{
+    if (array_numbers[index] == find)
+    {
+        Console.WriteLine($"Индекс числа {find} равен {index}");
+        break;
+    }
+    index++;
+}
