3fcc5031dde08c10c962f05e98923089c368a7f7
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 20:39:15 +0300

    04 Ex Write fill/print/index_of method

---
+++ C#/lecture/second/04 Example/Program.cs
@@ -0,0 +1,49 @@
+﻿void fill_array(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void print_array(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+    Console.Write($"\n");
+}
+
+int index_of(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array_numbers = new int[10];
+
+fill_array(array_numbers);
+print_array(array_numbers);
+
+Console.Write($"\n");
+
+int pos = index_of(array_numbers, 444);
+Console.WriteLine(pos);
