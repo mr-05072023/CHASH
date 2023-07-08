c6f670a2f1926b3eeff06d9a69483fb4f1001d27
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 15:15:26 +0300

    02 Ex Square and if/else

---
+++ C#/seminar/first/02 Example/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите два числа... ");
+int first_number = int.Parse(Console.ReadLine());
+int second_number = int.Parse(Console.ReadLine());
+
+int first_square = first_number* first_number;
+int second_square = second_number* second_number;
+
+if (first_number == second_square)
+{
+    Console.WriteLine($"{first_number} является квадратом числа {second_number}");
+}
+else if (second_number == first_square) 
+{
+    Console.WriteLine($"{second_number} является квадратом числа {first_number}");
+}
+else
+{
+    Console.WriteLine($"Нет чисел сответствующих условиям задания");
+}
