7ef51a76c53af0cb9486afd2ef1a1c4cbfad14d5
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 17:13:03 +0300

    02 T Find max number of three

---
+++ C#/task/first/02 Task/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите первое число... ");
+int first_number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число... ");
+int second_number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число... ");
+int third_number = int.Parse(Console.ReadLine());
+
+if (first_number > second_number)
+{
+    if (first_number > third_number)
+    {
+        Console.WriteLine($"{first_number} максимальное число");
+    }
+    else
+    {
+        Console.WriteLine($"{third_number} максимальное число");
+    }
+}
+else if (first_number < second_number)
+{
+    if (second_number > third_number)
+    {
+        Console.WriteLine($"{second_number} максимальное число");
+    }
+    else
+    {
+        Console.WriteLine($"{third_number} максимальное число");
+    }
+}
