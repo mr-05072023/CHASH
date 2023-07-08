a2a737e2c9382b9738c9eb301a11b75220e30398
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 16:30:35 +0300

    05 Ex Last number

---
+++ C#/seminar/first/05 Example/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число... ");
+int number = int.Parse(Console.ReadLine());
+int last_number = number % 10;
+Console.WriteLine($"Последняя цифра числа {number} равна {last_number}");
