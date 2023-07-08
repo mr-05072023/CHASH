cd0f45c145d7d0f6891b7c1cdfeed5856d57f664
mr-05072023 <mr.05072023@gmail.com>
2023-07-08 17:17:23 +0300

    02_01 T Search using an array

--- C#/task/first/02 Task/Program.cs
+++ C#/task/first/02 Task/Program.cs
@@ -1,31 +1,51 @@
-﻿Console.Write("Введите первое число... ");
-int first_number = int.Parse(Console.ReadLine());
+﻿// Console.Write("Введите первое число... ");
+// int first_number = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите второе число... ");
+// int second_number = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите третье число... ");
+// int third_number = int.Parse(Console.ReadLine());
+
+// if (first_number > second_number)
+// {
+//     if (first_number > third_number)
+//     {
+//         Console.WriteLine($"{first_number} максимальное число");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{third_number} максимальное число");
+//     }
+// }
+// else if (first_number < second_number)
+// {
+//     if (second_number > third_number)
+//     {
+//         Console.WriteLine($"{second_number} максимальное число");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{third_number} максимальное число");
+//     }
+// }
+// OR
+
+int[] array_numbers = new int[3];
+
+Console.Write("Введите первое число... ");
+array_numbers[0] = int.Parse(Console.ReadLine());
 
 Console.Write("Введите второе число... ");
-int second_number = int.Parse(Console.ReadLine());
+array_numbers[1] = int.Parse(Console.ReadLine());
 
 Console.Write("Введите третье число... ");
-int third_number = int.Parse(Console.ReadLine());
+array_numbers[2] = int.Parse(Console.ReadLine());
 
-if (first_number > second_number)
+int max_number = array_numbers[0];
+
+for (int i = 0; i < array_numbers.Length; i++)
 {
-    if (first_number > third_number)
-    {
-        Console.WriteLine($"{first_number} максимальное число");
-    }
-    else
-    {
-        Console.WriteLine($"{third_number} максимальное число");
-    }
+    if (max_number < array_numbers[i]) max_number = array_numbers[i];
 }
-else if (first_number < second_number)
-{
-    if (second_number > third_number)
-    {
-        Console.WriteLine($"{second_number} максимальное число");
-    }
-    else
-    {
-        Console.WriteLine($"{third_number} максимальное число");
-    }
-}
+Console.WriteLine($"{max_number} максимальное число");
