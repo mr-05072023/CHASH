ad23e31bda19b9b94f2ee91b4a40690c255af1e5
Mark <mr.05072023@gmail.com>
2023-07-13 23:52:54 +0300

    02 Ex Search through function

---
+++ C#/seminar/third/02 Example/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿int DefQuarter(int xPoint, int yPoint)
+{
+    if (xPoint > 0 && yPoint > 0) return 1;
+    if (xPoint < 0 && yPoint > 0) return 2;
+    if (xPoint < 0 && yPoint < 0) return 3;
+    if (xPoint > 0 && yPoint < 0) return 4;
+    return -1;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
+}
+
+int xPoint = GetInfo("Введите координату x ");
+int yPoint = GetInfo("Введите координату y ");
+
+Console.WriteLine($"Это {DefQuarter(xPoint, yPoint)} четверть!");
