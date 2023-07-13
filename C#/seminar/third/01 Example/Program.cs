53fee8a49218e3c07c904c2ca259f4c7ef14d2ab
Mark <mr.05072023@gmail.com>
2023-07-13 23:48:15 +0300

    01 Ex Search quarter

---
+++ C#/seminar/third/01 Example/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿Console.WriteLine("Введите координаты точки...");
+Console.Write("x = ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("y = ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine($"Точка [{x}, {y}] находится в 1-ой четверти.");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine($"Точка [{x}, {y}] находится во 2-ой четверти.");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine($"Точка [{x}, {y}] находится в 3-ей четверти.");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine($"Точка [{x}, {y}] находится в 4-ой четверти.");
+}
