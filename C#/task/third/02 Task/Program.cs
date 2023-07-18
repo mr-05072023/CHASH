5f9c3c649eeadd4391660ef21f0a4c72b30c11d0
Mark <mr.05072023@gmail.com>
2023-07-19 00:43:29 +0300

    02 T 3D Distance between two points

---
+++ C#/task/third/02 Task/Program.cs
@@ -0,0 +1,31 @@
+﻿int Square(int a)
+{
+    return a*a;
+}
+
+void SearchDistance()
+{
+    Console.WriteLine("Введите координаты точек...");
+    int[] pointArray = new int[6];
+    int count = 0;
+    for (int i = 1; i <= 2; i++)
+    {
+        Console.WriteLine($"Введите {i} точку...");
+        Console.Write($"x_{i} = ");
+        pointArray[count] = Convert.ToInt32(Console.ReadLine());
+        count++;
+        Console.Write($"y_{i} = ");
+        pointArray[count] = Convert.ToInt32(Console.ReadLine());
+        count++;
+        Console.Write($"z_{i} = ");
+        pointArray[count] = Convert.ToInt32(Console.ReadLine());
+        count++;
+    }
+    int xSquare = Square(pointArray[0] - pointArray[3]);
+    int ySquare = Square(pointArray[1] - pointArray[4]);
+    int zSquare = Square(pointArray[2] - pointArray[5]);
+    double distanceOne = Math.Sqrt(xSquare + ySquare + zSquare);
+    Console.WriteLine($"Дистанция от А({pointArray[0]}, {pointArray[1]}, {pointArray[2]}) до точки В({pointArray[3]}, {pointArray[4]}, {pointArray[5]}) равна {distanceOne}");
+}
+
+SearchDistance();
