9a662ebd3d6ec2f96ac34caaa13465337f1089a9
Mark <mr.05072023@gmail.com>
2023-07-19 00:42:20 +0300

    01 T It is palindrom?

---
+++ C#/task/third/01 Task/Program.cs
@@ -0,0 +1,26 @@
+﻿void ItISPalindrome(int number)
+{
+    int j = 1;
+    for (int i = 4; i >= 0; i--)
+    {
+        int firstNumber = (int) (number / (Math.Pow(10, i))) % 10;
+        int secondNumber = number % (int) Math.Pow(10, j) / (int) Math.Pow(10, j - 1);
+        j++;
+        if (firstNumber != secondNumber)
+        {
+            Console.WriteLine("Число не палиндром");
+            break;
+        }
+        if (i == 0)
+        {
+            Console.WriteLine("Число палиндром");
+        }
+    }
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+ItISPalindrome(GetInfo("Введите пятизначное число... "));
