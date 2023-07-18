34fd603de075e2d51852a09368e7d9542ac4d3c5
Mark <mr.05072023@gmail.com>
2023-07-19 00:45:27 +0300

    03 T Table cube

---
+++ C#/task/third/03 Task/Program.cs
@@ -0,0 +1,17 @@
+﻿void CalculateSquareTable(int number)
+{
+    for (int i = 1; i < number; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + ", ");
+    }
+    Console.WriteLine(number * number * number + "."); 
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+CalculateSquareTable(GetInfo("Введите число для подсчёта таблицы кубов... "));
+
