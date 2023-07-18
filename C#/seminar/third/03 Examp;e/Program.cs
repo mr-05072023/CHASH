17a268f8b8149b71803615f98223de257ed2242d
Mark <mr.05072023@gmail.com>
2023-07-18 23:22:05 +0300

    03 Ex Search range for quarter

---
+++ C#/seminar/third/03 Examp;e/Program.cs
@@ -0,0 +1,18 @@
+﻿string SearchInQuarter(int number)
+{
+    if (number == 1) return "для x [0; +∞), для y [0; +∞)" ;
+    if (number == 2) return "для x (-∞; 0], для y [0; +∞)" ;
+    if (number == 3) return "для x (-∞; 0], для y (-∞; 0]" ;
+    if (number == 4) return "для x [0; +∞), для y (-∞; 0]" ;
+    return "-1";
+}
+
+void VoiceDigital()
+{
+    Console.Write("Введите номер четверти...");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string value = SearchInQuarter(number);
+    Console.WriteLine($"В четверти {number} диапазон значений {value}");
+}
+
+VoiceDigital();
