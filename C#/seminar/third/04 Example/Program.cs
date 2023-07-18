21bac1b920d2aac2038bbfbf59fe87cf646ee106
Mark <mr.05072023@gmail.com>
2023-07-18 23:23:29 +0300

    04 Ex Distance between two points

---
+++ C#/seminar/third/04 Example/Program.cs
@@ -0,0 +1,27 @@
+﻿int Square(int a)
+{
+    return a*a;
+}
+
+void SearchDistance()
+{
+    Console.WriteLine("Введите координаты точек...");
+    int[] pointArray = new int[4];
+    int count = 0;
+    for (int i = 1; i <= 2; i++)
+    {
+        Console.WriteLine($"Введите {i} точку...");
+        Console.Write($"x_{i} = ");
+        pointArray[count] = Convert.ToInt32(Console.ReadLine());
+        count++;
+        Console.Write($"y_{i} = ");
+        pointArray[count] = Convert.ToInt32(Console.ReadLine());
+        count++;
+    }
+    int xSquare = Square(pointArray[0] - pointArray[2]);
+    int ySquare = Square(pointArray[1] - pointArray[3]);
+    double distance = Math.Sqrt(xSquare + ySquare);
+    Console.WriteLine($"Дистанция от А({pointArray[0]}, {pointArray[1]}) до точки В({pointArray[2]}, {pointArray[3]}) равна {distance}");
+}
+
+SearchDistance();
