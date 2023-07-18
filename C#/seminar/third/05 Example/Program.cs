8b76c5a0efafc59981b1b3c189255f10bd3fa1a5
Mark <mr.05072023@gmail.com>
2023-07-18 23:24:35 +0300

    05 Ex Table square

---
+++ C#/seminar/third/05 Example/Program.cs
@@ -0,0 +1,18 @@
+﻿void CalculateSquareTable(int number)
+{
+    for (int i = 1; i < number; i++)
+    {
+        Console.Write(Math.Pow(i, 2) + ", ");
+    }
+    Console.WriteLine(number * number + "."); 
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
+}
+
+CalculateSquareTable(GetInfo("Введите число для подсчёта таблицы квадратов... "));
+
