1a2bb678198153c44b30005aad7407791e82bc25
Mark <mr.05072023@gmail.com>
2023-07-22 20:45:22 +0300

    01 Ex Sum digit line

---
+++ C#/seminar/fourth/01 Example/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetSum(int num)
+{
+    int count = 0;
+    for (int i = 0; i <= num; i++)
+    {
+        count += i;
+    }
+    return count;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число... ");
+Console.WriteLine($"Сумма чисел от 1 до {num} равно {GetSum(num)}");
