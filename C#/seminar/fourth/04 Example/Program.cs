1e5e31005bc9e4b4b92461f96df9fe5c88f11cb3
Mark <mr.05072023@gmail.com>
2023-07-22 23:57:44 +0300

    04 Ex Print random binary array

---
+++ C#/seminar/fourth/04 Example/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] GetRandomBinaryArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++ )
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]\n");
+}
+
+Console.Write($"Массив из рандомных чисел от 0..=1 ");
+PrintArray(GetRandomBinaryArray());
