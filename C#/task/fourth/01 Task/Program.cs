c735a15640b8542a8a459ed33bb7ee95a6d24e40
Mark <mr.05072023@gmail.com>
2023-07-23 00:10:37 +0300

    01 T Exponentiation

---
+++ C#/task/fourth/01 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿int Exponentiation(int f_num, int s_num)
+{
+    int count = 1;
+    for (int i = 1; i <= s_num; i++ )
+    {
+        count *= f_num;
+    }
+    return count;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int f_num = GetInfo("Введите число A... ");
+int s_num = GetInfo("Введите число B... ");
+Console.WriteLine($"Число {f_num} в натуральной степени {s_num} равно {Exponentiation(f_num, s_num)}");
