953f6481596cef9eacead5cf1d68ed30357a3f0d
Mark <mr.05072023@gmail.com>
2023-07-23 00:29:02 +0300

    03 T Print random and custom array

---
+++ C#/task/fourth/03 Task/Program.cs
@@ -0,0 +1,44 @@
+﻿void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int[] GetRandomArray(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(0, 101);
+    }
+    return array;
+}
+
+int[] GetArray(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = GetInfo($"Введите {i} число массива... ");
+    }
+    return array;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число значений в массиве... ");
+PrintArray(GetArray(num));
+Console.WriteLine("Рандомный товарищ");
+PrintArray(GetRandomArray(num));
