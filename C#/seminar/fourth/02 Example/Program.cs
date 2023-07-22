58c79a30ca3dad9940e5792383f280cad70a0858
Mark <mr.05072023@gmail.com>
2023-07-22 20:46:47 +0300

    02 Ex Count digit in number

---
+++ C#/seminar/fourth/02 Example/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetCount(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        if (number / 10 >= 0)
+        {
+            number /= 10;
+            count +=1;
+        }
+    }
+    return count;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число... ");
+Console.WriteLine($"Количество цифр в числе {num} равно {GetCount(num)}");
