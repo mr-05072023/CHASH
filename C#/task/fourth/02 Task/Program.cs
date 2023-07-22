45f9285944e8e3e7be4022d222c4f184a7f1e02a
Mark <mr.05072023@gmail.com>
2023-07-23 00:16:37 +0300

    02 T Sum digit in the number

---
+++ C#/task/fourth/02 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetSum(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count += num % 10;
+        num /= 10;
+    }
+    return count;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int f_num = GetInfo("Введите число ... ");
+Console.WriteLine($"Сумма цифр числа {f_num} равняется {GetSum(f_num)}");
