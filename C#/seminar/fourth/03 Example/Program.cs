68126626a47912c315b1a067ea148c257eec8023
Mark <mr.05072023@gmail.com>
2023-07-22 20:53:34 +0300

    03 Ex Multiplication number

---
+++ C#/seminar/fourth/03 Example/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetSum(int num)
+{
+    int count = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        count *= i;
+    }
+    return count;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число... ");
+Console.WriteLine($"Сумма чисел от 1 до {num} равно {GetSum(num)}");
