5a219f753890bb9a94201ab3c4e1ca7963aaa2aa
Mark <mr.05072023@gmail.com>
2023-07-23 23:36:17 +0300

    01_01 Ex Range selection

--- C#/seminar/fifth/01 Example/Program.cs
+++ C#/seminar/fifth/01 Example/Program.cs
@@ -1,11 +1,11 @@
-﻿object[] GetDowblArray(int len_array)
+﻿object[] GetDowblArray(int len_array, int beginValue, int endValue)
 {
     int[] array = new int[len_array];
     int[] arrayNP = new int[2];
     object[] result = {array, arrayNP};
     for (int i = 0; i < len_array; i++)
     {
-        array[i] = new Random().Next(-9, 10);
+        array[i] = new Random().Next(beginValue, endValue);
         if (array[i] > 0)
         {
             arrayNP[1] += array[i];
@@ -39,7 +39,10 @@ int GetInfo(string message)
 }
 
 int num = GetInfo("Введите размер массива... ");
-int[] array = (int[]) GetDowblArray(num)[0];
-int[] array_value = (int[]) GetDowblArray(num)[1];
-Console.Write($"Сумма положительных чисел массива "); PrintArray(array); Console.Write($"равна {array_value[1]}\n");
-Console.Write($"Сумма отрицательных чисел массива "); PrintArray(array); Console.Write($"равна {array_value[0]}\n");
+int beginValue = GetInfo("Введите начало диапазона вариантов... ");
+int endValue = GetInfo("Введите конец диапазона вариантов... ");
+object[] array = GetDowblArray(num, beginValue, endValue);
+int[] array_first = (int[]) array[0];
+int[] array_value = (int[]) array[1];
+Console.Write($"Сумма положительных чисел массива "); PrintArray(array_first); Console.Write($"равна {array_value[1]}\n");
+Console.Write($"Сумма отрицательных чисел массива "); PrintArray(array_first); Console.Write($"равна {array_value[0]}\n");
