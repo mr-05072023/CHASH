205825f49a3a025e03fa864759dfe26065d0b079
Mark <mr.05072023@gmail.com>
2023-07-23 23:10:06 +0300

    02 Ex Reverse array value

---
+++ C#/seminar/fifth/02 Example/Program.cs
@@ -0,0 +1,38 @@
+﻿object[] ReversValueArray(int num)
+{
+ int[] firstArray = new int[num];
+ int[] reversArray = new int[num];
+ object[] result = {firstArray, reversArray};
+ for (int i = 0; i < num; i++)
+ {
+    firstArray[i] = new Random().Next(-9, 10);
+    reversArray[i] = -firstArray[i];
+ }
+ return result;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("] ");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите длину массива... ");
+object[] array = ReversValueArray(num);
+int[] greatArray = (int[]) array[0];
+int[] reversArray = (int[]) array[1];
+PrintArray(greatArray); Console.Write(" -> "); PrintArray(reversArray); Console.WriteLine("");
