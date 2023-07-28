af1f2902904f4eaad7d7ebfbabc41b5872d394ae
Mark <mr.05072023@gmail.com>
2023-07-29 00:40:33 +0300

    03 T Substruction from max value array min value

---
+++ C#/task/fifth/03 Task/Program.cs
@@ -0,0 +1,39 @@
+﻿int SubstractionMaxMinValueInArray(int num, int beginValue, int endValue)
+{
+    int[] array = new int[num];
+    int maxValue = int.MinValue;
+    int minValue = int.MaxValue;
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(beginValue, endValue);
+        if (array[i] > maxValue) maxValue = array[i];
+        if (array[i] < minValue) minValue = array[i];
+    }
+    PrintArray(array);
+    return maxValue - minValue;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("] -> ");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите длину массива... ");
+int beginValue = GetInfo("Введите начало диапазона вариантов... ");
+int endValue = GetInfo("Введите конец диапазона вариантов... ");
+Console.WriteLine($"{SubstractionMaxMinValueInArray(num, beginValue, endValue)}");
