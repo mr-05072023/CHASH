a2dfb920fbc11e297d49da697487f528e9bd975d
Mark <mr.05072023@gmail.com>
2023-07-30 20:55:27 +0300

    05 Ex Copy array

---
+++ C#/seminar/sixth/05 Example/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CopyArray(int num, int beginValue, int endValue)
+{
+    int[] array = new int[num];
+    int[] copyArray = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(beginValue, endValue);
+        copyArray[i] = array[i];
+    }
+    PrintArray(array);
+    return copyArray;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите длину массива... ");
+int beginValue = GetInfo("Введите начало диапазона вариантов... ");
+int endValue = GetInfo("Введите конец диапазона вариантов... ");
+PrintArray(CopyArray(num, beginValue, endValue));
