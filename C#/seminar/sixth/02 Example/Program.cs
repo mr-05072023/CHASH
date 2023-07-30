3bf49291fc28e2583daf813dfc17cab495213be7
Mark <mr.05072023@gmail.com>
2023-07-30 20:52:00 +0300

    02 Ex Equality triangle

---
+++ C#/seminar/sixth/02 Example/Program.cs
@@ -0,0 +1,18 @@
+﻿bool EqualityTriangle(int firstSide, int secondSide, int thirdSide)
+{
+    if (firstSide + secondSide < thirdSide) return false;
+    if (firstSide + thirdSide < secondSide) return false;
+    if (thirdSide + secondSide < firstSide) return false;
+    return true;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int firstSide = GetInfo("Введите первую сторону... ");
+int secondSide = GetInfo("Введите вторую сторону... ");
+int thirdSide = GetInfo("Введите третью сторону... ");
+Console.WriteLine($"Треугольник со сторонами {firstSide}, {secondSide}, {thirdSide} -> {EqualityTriangle(firstSide, secondSide, thirdSide)}");
