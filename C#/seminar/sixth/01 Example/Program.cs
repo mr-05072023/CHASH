e73eb5473272a65d71a997861b3e1a2bf43c1313
Mark <mr.05072023@gmail.com>
2023-07-30 19:46:02 +0300

    01 Ex Reverse array

---
+++ C#/seminar/sixth/01 Example/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] ReverseArray(int num, int beginValue, int endValue)
+{
+    int[] array = new int[num];
+    int[] reversArray = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(beginValue, endValue);
+        reversArray[num - i - 1] = array[i];
+    }
+    PrintArray(array);
+    return reversArray;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите длину массива... ");
+int beginValue = GetInfo("Введите начало диапазона вариантов... ");
+int endValue = GetInfo("Введите конец диапазона вариантов... ");
+PrintArray(ReverseArray(num, beginValue, endValue));
