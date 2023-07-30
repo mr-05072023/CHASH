468dcd6f721664774d9a4638c40a2c343f441595
Mark <mr.05072023@gmail.com>
2023-07-30 20:54:36 +0300

    04 Ex Fibonacci sequence

---
+++ C#/seminar/sixth/04 Example/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] FibonacciSequence(int num)
+{
+    int[] fibinacci = new int[num];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+    return fibonacci;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+        if (i != array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]\n");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число... ");
+PrintArray(FibonacciSequence(num));
