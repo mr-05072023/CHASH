369b86c2a9742af8accaab9f0eee23d54a30b028
Mark <mr.05072023@gmail.com>
2023-07-30 20:53:42 +0300

    03 Ex Binary performance

---
+++ C#/seminar/sixth/03 Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+string BinaryPerformance(int num)
+{
+    string binarNum = String.Empty;
+    while (num != 0)
+    {
+        binarNum = num % 2 + binarNum;
+        num /= 2;
+    }
+    return binarNum;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInfo("Введите число... ");
+Console.WriteLine($"{BinaryPerformance(num)}");
