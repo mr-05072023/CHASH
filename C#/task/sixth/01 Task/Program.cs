4b64b85555bb31c9d288789c0bed90b40d60526d
Mark <mr.05072023@gmail.com>
2023-07-30 21:27:53 +0300

    01 T More zero

---
+++ C#/task/sixth/01 Task/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetQuantityMoreZero()
+{
+    int count = 0;
+    while(true)
+    {   
+        Console.WriteLine("Введите число для продолжения работы, или символ(ы) для завершения работы...");
+        string str = Console.ReadLine();
+        if (int.TryParse(str, out int num)) { if (num > 0) count += 1; continue; }
+        Console.Write("... Вы прервали ход программы, результат -> ");
+        break;
+    }
+    return count;
+}
+Console.WriteLine($"{GetQuantityMoreZero()}");
