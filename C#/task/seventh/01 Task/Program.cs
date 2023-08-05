9e5e1312cfb7012ff309820e6e0c15338e0add58
Mark <mr.05072023@gmail.com>
2023-08-06 01:05:45 +0300

    01 T Get double array have real value

---
+++ C#/task/seventh/01 Task/Program.cs
@@ -0,0 +1,45 @@
+﻿double[,] GetDoubleArray(int line, int column, int beginValue, int endValue)
+{
+    double[,] array = new double[line, column];
+    Random rand = new Random();
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = rand.Next(beginValue, endValue) + rand.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintDoubleArray(double[,] array)
+{   
+    int line = array.GetLength(0);
+    int column = array.GetLength(1);
+    for (int i = 0; i < line; i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{array[i, j]}");
+            if (j != column - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    } 
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int line = GetInfo("Введите количество строк массива... ");
+int column = GetInfo("Введите количество столбцов массива... ");
+int beginValue = GetInfo("Введите начало диапазона вариантов... ");
+int endValue = GetInfo("Введите конец диапазона вариантов... ");
+PrintDoubleArray(GetDoubleArray(line, column, beginValue, endValue));
