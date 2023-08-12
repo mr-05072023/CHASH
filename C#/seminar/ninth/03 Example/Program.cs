b9675696f668b49753f9f196b53039a97d0f3fa1
Mark <mr.05072023@gmail.com>
2023-08-13 00:16:57 +0300

    03 Ex Get sum use recursive method

---
+++ C#/seminar/ninth/03 Example/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetSum(int num)
+{
+    if (num != 0) return num % 10 + GetSum(num / 10);
+    return 0;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = GetInfo("Введите число для подсчёта суммы его цифр... ");
+Console.WriteLine($"Сумма цифр числа = {GetSum(num)}");
