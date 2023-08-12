6776fb9f7869743016686076c5afe17fca23b944
Mark <mr.05072023@gmail.com>
2023-08-13 00:13:59 +0300

    01 Ex Get sequence of natural numbers

---
+++ C#/seminar/ninth/01 Example/Program.cs
@@ -0,0 +1,19 @@
+﻿void GetSequenceOfNaturalNumbers(int temporary, int constant)
+{
+    if (temporary == 1) Console.Write($"\"");
+    Console.Write($"{temporary}");
+    if (temporary < constant) Console.Write($", ");
+    if (temporary != constant) GetSequenceOfNaturalNumbers(temporary + 1, constant);
+    if (temporary == constant) Console.Write($"\"");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numBeg = 1;
+int constant = GetInfo("Введите конечное число последовательности натуральных чисел... ");
+GetSequenceOfNaturalNumbers(numBeg, constant);
+Console.WriteLine();
