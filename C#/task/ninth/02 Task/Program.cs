8640512b865d316ae66d9f61cd628c76ef361299
Mark <mr.05072023@gmail.com>
2023-08-13 01:05:51 +0300

    02 T Sum of sequence numbers

---
+++ C#/task/ninth/02 Task/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetSumOfSequenceNumbers(int M, int N)
+{
+    if (M != N + 1) return M + GetSumOfSequenceNumbers(M + 1, N);
+    return 0;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int M = GetInfo("Введите начало последовательности... ");
+int N = GetInfo("Введите окончание последовательности... ");
+Console.WriteLine($"Сумма чисел последовательности от {M} до {N} = {GetSumOfSequenceNumbers(M, N)}");
