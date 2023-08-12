af8f2db7f282bd359c36f947e91a360ed42f5df3
Mark <mr.05072023@gmail.com>
2023-08-13 00:15:57 +0300

    02 Ex Get sequence of natural numbers on distance

---
+++ C#/seminar/ninth/02 Example/Program.cs
@@ -0,0 +1,19 @@
+﻿void GetSequenceOfNaturalNumbers(int numBeg, int numFin, int temporary)
+{  
+    if (temporary == numBeg) Console.Write($"\"");
+    Console.Write($"{temporary}");
+    if (temporary < numFin) Console.Write($", ");
+    if (temporary != numFin) GetSequenceOfNaturalNumbers(numBeg, numFin, temporary + 1);
+    if (temporary == numFin) Console.Write($"\"");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int numBeg = GetInfo("Введите начальное число последовательности натуральных чисел... ");
+int numFin = GetInfo("Введите конечное число последовательности натуральных чисел... ");
+int temporary = numBeg;
+GetSequenceOfNaturalNumbers(numBeg, numFin, temporary);
+Console.WriteLine();
