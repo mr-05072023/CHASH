94c60a7a0102e3ce79df83dd2910b09979f371be
Mark <mr.05072023@gmail.com>
2023-08-13 01:06:37 +0300

    03 T Get Ackerman function

---
+++ C#/task/ninth/03 Task/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetAckermanFunction(int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M > 0 && N == 0) return GetAckermanFunction(M - 1, 1);
+    if (M > 0 && N > 0) return GetAckermanFunction(M - 1, GetAckermanFunction(M, N - 1));
+    return 1;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int M = GetInfo("Введите M... ");
+int N = GetInfo("Введите N... ");
+Console.WriteLine($"M = {M}, N = {N} -> A(M, N) = {GetAckermanFunction(M, N)}");
