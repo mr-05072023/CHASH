235a4ccf0b1caa51cca70e5444614d96c989ba0f
Mark <mr.05072023@gmail.com>
2023-08-13 01:01:57 +0300

    01 T Get reverse order of natural numbers

---
+++ C#/task/ninth/01 Task/Program.cs
@@ -0,0 +1,19 @@
+﻿void GetReverseOrderOfNaturalNumbers(int temporary, int constant)
+{
+    if (temporary == constant) Console.Write($"\"");
+    if (temporary != constant) GetReverseOrderOfNaturalNumbers(temporary + 1, constant);
+    Console.Write($"{temporary}");
+    if (temporary <= constant && temporary != 1) Console.Write($", ");
+    if (temporary == 1) Console.Write($"\"");
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numBeg = 1;
+int constant = GetInfo("Введите конечное число последовательности натуральных чисел... ");
+GetReverseOrderOfNaturalNumbers(numBeg, constant);
+Console.WriteLine();
