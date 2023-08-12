9dc25dd376e5905c2ca3e9d6dd13c4f48fd6534f
Mark <mr.05072023@gmail.com>
2023-08-13 00:18:28 +0300

    04 Ex Get exponentiation use recursive method

---
+++ C#/seminar/ninth/04 Example/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetExponentiation(int base_, int exponent)
+{
+    if (exponent != 0) return base_ * GetExponentiation(base_, exponent - 1);
+    return 1;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int base_ = GetInfo("Введите основание числа... ");
+int exponent = GetInfo("Введите основание числа... ");
+Console.WriteLine($"Число {base_} возведённое в степень {exponent} = {GetExponentiation(base_, exponent)}");
